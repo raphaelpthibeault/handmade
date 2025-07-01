136dd3af2a63192ee27493dd33cb28212f3c388f
rpthibeault <rpthibeault@outlook.com>
2025-07-01 19:48:06 -0400

    allocated a backbuffer

--- x11_handmade.c
+++ x11_handmade.c
@@ -4,26 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <err.h>
+#include <stddef.h>
+#include <stdint.h>
 
 static Display *display;
 static int screen;
 static Window root;
-
-static Window
-CreateWindow(int x, int y, int w, int h, int b)
-{
-	Window win;
-	XSetWindowAttributes xwa;
-
-	xwa.background_pixel = WhitePixel(display, screen);
-	xwa.border_pixel = BlackPixel(display, screen);
-	xwa.event_mask = ExposureMask | Button1MotionMask | ButtonPressMask | ButtonReleaseMask | KeyPressMask;
-
-	win = XCreateWindow(display, root, x, y, w, h, b, DefaultDepth(display, screen), 
-			InputOutput, DefaultVisual(display, screen), CWBackPixel | CWBorderPixel | CWEventMask, &xwa);
-
-	return win;
-}
 
 static GC
 CreateGC(int lineWidth)
@@ -47,10 +33,67 @@ CreateGC(int lineWidth)
 	return gc;
 }
 
+static Window
+CreateWindow(int x, int y, int width, int height, int b)
+{
+	Window win;
+	XSetWindowAttributes xwa;
+
+	xwa.background_pixel = WhitePixel(display, screen);
+	xwa.border_pixel = BlackPixel(display, screen);
+	xwa.event_mask = ExposureMask | StructureNotifyMask;
+
+	win = XCreateWindow(display, root, x, y, width, height, b, DefaultDepth(display, screen), 
+			InputOutput, DefaultVisual(display, screen), CWBackPixel | CWBorderPixel | CWEventMask, &xwa);
+
+	return win;
+}
+
 static bool Running;
 
+static Pixmap backbuffer = 0;
+void *bitmapMemory = NULL;
+static XImage *bitmapHandle = NULL;
+
+static void
+UpdateWindow(Window w, GC gc, int width, int height)
+{
+	XPutImage(display, backbuffer, gc, bitmapHandle, 0, 0, 0, 0, width, height);
+	/* rectangle to rectangle copy */
+	XCopyArea(display, backbuffer, w, gc, 0, 0, width, height, 0, 0);	
+}
+
+/* resize bitmap meant to represent Window w */
+static void
+ResizeBitmap(Window w, int width, int height)
+{
+	printf("Resize Bitmap with %dx%d\n", width, height);
+
+	if (bitmapHandle)
+	{
+		/* NOTE: Also frees the bitmapMemory */
+		XDestroyImage(bitmapHandle);
+		bitmapHandle = NULL;
+	}
+	if (backbuffer)
+	{
+		XFreePixmap(display, backbuffer);
+		backbuffer = 0;
+	}
+
+	bitmapMemory = malloc(sizeof(uint32_t) * width * height); // ARGB
+	bitmapHandle = XCreateImage(display, DefaultVisual(display, screen), DefaultDepth(display, screen), ZPixmap, 0, (char *)bitmapMemory, width, height, 32, 0); // 32 bitmap pad?  0 bytes per line?
+	backbuffer = XCreatePixmap(display, w, width, height, DefaultDepth(display, screen));
+}
+
+
+/* TODO Free everything at the end */
+
+#define DEFAULT_WIDTH 1920
+#define DEFAULT_HEIGHT 1080
+
 int
-main(int argc, char *argv[])
+main(void)
 {
 	Window mainWindow;
 	XEvent event;
@@ -64,17 +107,19 @@ main(int argc, char *argv[])
 	screen = DefaultScreen(display);
 	root = RootWindow(display, screen);
 
-	mainWindow = CreateWindow(50, 50, 1920, 1080, 15);
+	mainWindow = CreateWindow(1, 1, DEFAULT_WIDTH, DEFAULT_HEIGHT, 0);
 	XStoreName(display, mainWindow, "Handmade");
 	XMapWindow(display, mainWindow);
 
-	gc = CreateGC(4); // lineWidth=4
+	gc = CreateGC(4); // lineWidth=4, why? idk
+
+	ResizeBitmap(mainWindow, DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
 	wmDeleteMessage = XInternAtom(display, "WM_DELETE_WINDOW", False);
 	XSetWMProtocols(display, mainWindow, &wmDeleteMessage, 1);
 
 	/* event loop */
-	int init = 0, prevX = 0, prevY = 0;
+	int prevWidth = DEFAULT_WIDTH, prevHeight = DEFAULT_HEIGHT;
 	Running = true;
 	while (Running)
 	{
@@ -82,32 +127,26 @@ main(int argc, char *argv[])
 		switch (event.type)
 		{
 			case Expose:
-				printf("Exposure\n");
+				//printf("Expose (force redraw)\n");
+				/* Is this my problem? */
 				break;
-			case ConfigureNotify: /* triggered when the window is resized, moved, or restacked */
+			case ConfigureNotify: /* triggered when window is resized, moved, or restacked */
+				/* don't have to update the  mainWindow, that's done automatically */
 				XConfigureEvent *xc = &event.xconfigure;
-				printf("Resize or Move: New size is %dx%d\n", xc->width, xc->height);
-				break;
-			case ButtonPress:
-				XDrawPoint(display, event.xbutton.window, gc, event.xbutton.x, event.xbutton.y);
-				printf("Button Press\n");
-				break;
-			case MotionNotify:
-				if (init)
+				if (xc->x < 0 || xc-> y < 0) 
 				{
-					XDrawLine(display, event.xbutton.window, gc, prevX, prevY, event.xbutton.x, event.xbutton.y);
+					/* not my problem */
+					break;
 				}
-				else
+
+				printf("want window  %dx%d @ %dx%d\n", xc->width, xc->height, xc->x, xc->y);	
+				if (prevWidth != xc->width || prevHeight != xc->height)
 				{
-					XDrawPoint(display, event.xbutton.window, gc, event.xbutton.x, event.xbutton.y);
-					init = 1;
+					ResizeBitmap(mainWindow, xc->width, xc->height);
+					prevWidth = xc->width;
+					prevHeight = xc->height;
 				}
-				prevX = event.xbutton.x;
-				prevY = event.xbutton.y;
-				break;
-			case ButtonRelease:
-				init = 0;
-				printf("Button Release\n");
+
 				break;
 			case ClientMessage:
 				if (event.xclient.data.l[0] == wmDeleteMessage)
@@ -119,7 +158,18 @@ main(int argc, char *argv[])
 		}
 	}
 
-cleanup:
+	if (bitmapHandle)
+	{
+		/* NOTE: Also frees the bitmapMemory */
+		XDestroyImage(bitmapHandle);
+		bitmapHandle = NULL;
+	}
+	if (backbuffer)
+	{
+		XFreePixmap(display, backbuffer);
+		backbuffer = 0;
+	}
+
 	XUnmapWindow(display, mainWindow);
 	XFreeGC(display, gc);
 	XDestroyWindow(display, mainWindow);
